6ef34767af94fa2826dbb6a2d9b83eec3bb1d13c
khbesbes <5786@holbertonstudents.com>
2023-08-16 18:17:19 +0100

    correction of doc

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,21 +6,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes= flase)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-        """ display “Hello HBNB!” """
-        return 'Hello HBNB!'
+    """ display “Hello HBNB!” """
+    return 'Hello HBNB!'
 
-@app.route('/', strict_slashes= flase)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-        """ display “ HBNB!” """
-        return 'HBNB!'
+    """ display “HBNB” """
+    return 'HBNB'
 
-@app.route('c/<text>', strict_slashes= flase)
+
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
-         """ display “C ” followed by the value of the text variable """
-         return 'C {}'.format(text.replace('_', ' '))
+    """ display “C ” followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)

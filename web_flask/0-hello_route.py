614e380b3d64f2e342ac583061ad5ccb20fb0446
khbesbes <5786@holbertonstudents.com>
2023-08-16 15:44:12 +0100

    hello file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application """
+"""
+    starts a Flask web application
+"""
 from flask import Flask
 
 
@@ -8,14 +10,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ display “Hello HBNB!” """
+    """
+        display “Hello HBNB!”
+    """
     return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ display “HBNB” """
-    return 'HBNB'
 
 
 if __name__ == '__main__':
